fda3c3243a6368900e040dd6c55fec695171d6a2
HamzaAryanSapnil <139946462+HamzaAryanSapnil@users.noreply.github.com>
2024-11-11 14:59:00 +0600

    responsive swiper slider

--- app/page.js
+++ app/page.js
@@ -1,101 +1,107 @@
-import Image from "next/image";
+"use client";
+import { Swiper, SwiperSlide } from "swiper/react";
 
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+
+import "./style.css";
+
+// import required modules
+import { Navigation } from "swiper/modules";
+import { useRef, useState } from "react";
 export default function Home() {
+    const prevRef = useRef(null);
+    const nextRef = useRef(null);
+    const swiperRef = useRef(null);
+    const [isBeginning, setIsBeginning] = useState(true);
+    const [isEnd, setIsEnd] = useState(false);
+  const images = [
+    {
+      img: "https://i.ibb.co.com/vxcbTsg/cat-s-care.jpg",
+      title: "First Image",
+    },
+    {
+      img: "https://i.ibb.co.com/Pm5DszC/amazing-bengal-tiger-nature.jpg",
+      title: "Second Image",
+    },
+    {
+      img: "https://i.ibb.co.com/vxcbTsg/cat-s-care.jpg",
+      title: "Third Image",
+    },
+    {
+      img: "https://i.ibb.co.com/vxcbTsg/cat-s-care.jpg",
+      title: "Fourth Image",
+    },
+  ];
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <main className="relative">
+      <Swiper
+        navigation={{
+          prevEl: prevRef.current,
+          nextEl: nextRef.current,
+        }}
+        onInit={(swiper) => {
+          swiper.params.navigation.prevEl = prevRef.current;
+          swiper.params.navigation.nextEl = nextRef.current;
+          swiper.navigation.init();
+          swiper.navigation.update();
+        }}
+        onSlideChange={(swiper) => {
+          setIsBeginning(swiper.isBeginning);
+          setIsEnd(swiper.isEnd);
+        }}
+        modules={[Navigation]}
+        className="mySwiper"
+      >
+        {images.map((item, index) => (
+          <SwiperSlide key={index}>
+            <div
+              style={{
+                backgroundImage: `url(${item.img})`,
+                // backgroundRepeat: "no-repeat",
+                // backgroundPosition: "center",
+                // backgroundSize: "cover",
+              }}
+              className="h-[550px] w-full flex justify-center items-center bg-cover bg-center bg-no-repeat md:h-[400px] sm:h-[300px]"
+            >
+              <h2 className="text-white text-lg font-bold bg-black/60 p-2 rounded-lg">
+                {item.title}
+              </h2>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+      {/* Custom Navigation Buttons */}
+      <div className="absolute right-4 bottom-4 flex space-x-2 z-10">
+        <button
+          ref={prevRef}
+          onClick={() => swiperRef.current?.slidePrev()}
+          className={`p-3 rounded-full  bg-black bg-opacity-25 transition 
+            ${
+              isBeginning
+                ? "bg-gray-400 cursor-not-allowed"
+                : " text-white hover:bg-orange-600"
+            }`}
+          disabled={isBeginning}
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          Prev
+        </button>
+        <button
+          onClick={() => swiperRef.current?.slideNext()}
+          ref={nextRef}
+          className={`p-3 rounded-full backdrop-blur-sm transition 
+            ${
+              isEnd
+                ? "bg-gray-400 cursor-not-allowed"
+                : "bg-black/25 text-white hover:bg-orange-600"
+            }`}
+          disabled={isEnd}
         >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+          Next
+        </button>
+      </div>
+    </main>
   );
 }
